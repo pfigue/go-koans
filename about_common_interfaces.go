5d34bfd346f04b64204ad864c9d6ea5d7ddd5088
Pablo Figue <pfigue@posteo.de>
2016-11-07 13:27:59 +0100

    It wins life.

--- about_common_interfaces.go
+++ about_common_interfaces.go
@@ -1,6 +1,7 @@
 package go_koans
 
 import "bytes"
+import "io"
 
 func aboutCommonInterfaces() {
 	{
@@ -18,6 +19,7 @@ func aboutCommonInterfaces() {
 		   $ open http://localhost:8080/pkg/bytes/
 		*/
 
+    in.WriteTo(out)
 		assert(out.String() == "hello world") // get data from the io.Reader to the io.Writer
 	}
 
@@ -27,6 +29,7 @@ func aboutCommonInterfaces() {
 
 		out := new(bytes.Buffer)
 
+    io.CopyN(out, in, 5)
 		assert(out.String() == "hello") // duplicate only a portion of the io.Reader
 	}
 }
